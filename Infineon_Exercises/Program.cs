1aef114cad954f2b308f28e23e4c1b20ecbb9f7d
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-08-02 12:52:35 +0200

    Add project files.

---
+++ Infineon_Exercises/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Infineon_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solution
+            int[] checker = new int[100];
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int x = new Random().Next(1, 101);
+                if (checker.Contains(x))
+                {
+                    i--;
+                }
+                else
+                {
+                    checker[i - 1] = x;
+                    Console.WriteLine(x);
+                    if (x % 7 == 0) Console.WriteLine("Lucky number!");
+                    if (i % 5 == 0) Console.WriteLine("---");
+                }
+            }
+
+        }
+    }
+}
